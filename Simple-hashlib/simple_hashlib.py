d3da0c4bbf077df2dc67237cbface5e02757cf96
tvylormvde <wernerowy@gmail.com>
2021-02-12 13:31:20 +0100

    Simple hashlib solution

---
+++ Simple-hashlib/simple_hashlib.py
@@ -0,0 +1,19 @@
+# from hashlib import md5, sha224, sha256, sha384, sha512, sha1
+import hashlib
+
+def checkio(password, algorithm):
+    algorithms = ("md5", "sha224", "sha256", "sha384", "sha512", "sha1")
+    password = bytes(password, 'utf-8')
+    if algorithm in algorithms:
+        return getattr(hashlib, algorithm)(password).hexdigest()
+    return None
+
+    
+
+
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio('welcome', 'md5') == '40be4e59b9a2a2b5dffb918c0e86b3d7'
+    assert checkio(
+        'happy spam', 'sha224') == '6e9dc3e01d57f1598c2b40ce59fc3527e698c77b15d0840ae96a8b5e'
