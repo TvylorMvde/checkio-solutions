8b0697f2f6a88b8005ad48599d09f3dffedf3487
tvylormvde <wernerowy@gmail.com>
2021-02-12 14:00:35 +0100

    Revert "Similar triangles solution"
    This reverts commit 1c8a73d9a6c1c661a9b8f2b9e6b9ddfd28045ef5.

--- script.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-def foo(a, b):
-    print("Tutaj jest wpisany pierwszy argument: {}".format(a))
-    print("Tutaj jest wpisany drugi argument: {}".format(b))
-    
-
-if __name__ == "__main__":
-    foo(*sys.argv[1:])
--- testing.py
+++
@@ -1,3 +0,0 @@
-test = "19.09.1979"
-
-print(int(test.split('.')[-1]))
