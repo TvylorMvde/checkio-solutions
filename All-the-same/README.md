08062b26e6bcfdc303ef1e3e599cc1eb0774d289
TvylorMvde <wernerowy@gmail.com>
2021-02-14 13:54:19 +0100

    All the Same solutions

---
+++ All-the-same/README.md
@@ -0,0 +1,7 @@
+In this mission you should check if all elements in the given list are equal.
+
+<b>Input:</b> List.
+
+<b>Output:</b> Bool.
+
+The idea for this mission was found on Python Tricks series by Dan Bader
---
+++ All-the-same/all_the_same.py
@@ -0,0 +1,18 @@
+from collections import Counter
+
+
+def all_the_same(elements):
+    return True if len(Counter(elements)) in [0, 1] else False
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(all_the_same([1, 1, 1]))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert all_the_same([1, 1, 1]) == True
+    assert all_the_same([1, 2, 1]) == False
+    assert all_the_same(['a', 'a', 'a']) == True
+    assert all_the_same([]) == True
+    assert all_the_same([1]) == True
+    print("Coding complete? Click 'Check' to earn cool rewards!")
