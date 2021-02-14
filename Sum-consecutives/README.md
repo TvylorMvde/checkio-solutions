5a4bbc5575ebf57583bf07e5836f84048057b56e
TvylorMvde <wernerowy@gmail.com>
2021-02-14 14:27:24 +0100

    Sum consecutives solution

---
+++ Sum-consecutives/README.md
@@ -0,0 +1,11 @@
+You are given a list that contains solely integers (positive and negative). Your job is to sum only the numbers that are identical and consecutive.
+
+<b>Input:</b> a list.
+
+<b>Output:</b> an iterable.
+
+<b>Example:</b>
+```
+sum_consecutives([1, 1, 1, 1]) == [4]
+sum_consecutives([1, 1, 2, 2]) == [2, 4]
+```
---
+++ Sum-consecutives/sum_consecutives.py
@@ -0,0 +1,20 @@
+from itertools import groupby
+
+
+def sum_consecutives(numbers):
+    result = [(number, sum(1 for i in count)) for number, count in groupby(numbers)]
+    return [x * y for (x, y) in result]
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(list(sum_consecutives([1, 1, 1, 1])))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert list(sum_consecutives([1, 1, 1, 1])) == [4]
+    assert list(sum_consecutives([1, 1, 2, 2])) == [2, 4]
+    assert list(sum_consecutives([1, 1, 2, 1])) == [2, 2, 1]
+    assert list(sum_consecutives([3, 3, 3, 4, 4, 5, 6, 6])) == [9, 8, 5, 12]
+    assert list(sum_consecutives([1])) == [1]
+    assert list(sum_consecutives([])) == []
+    print("Coding complete? Click 'Check' to earn cool rewards!")
